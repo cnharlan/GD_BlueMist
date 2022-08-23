7395f963e67c1b1110aedd2a94558d67828407d5
cnharlan <cnharlan@gmail.com>
2022-08-22 23:34:01 -0400

    003 - Added PSX Shader
    -Imported PSX Shader Project Files
    -Began creating materials, plugging in shaders to test pipeline
    -Using TestMap to view progress
    -Deleted Camera in PSX Demo World. Need to reference original in order to reference settings.

---
+++ PSX Shader/post_process/options.gd
@@ -0,0 +1,51 @@
+extends Node
+
+#public variables to hold PSX materials
+export(Material) var post_process_lcd
+export(Material) var post_process_blur
+export(Material) var post_process_dither_band
+#public var to hold instance of environment node
+export(Environment) var environment
+
+#At start of scene, set inherited value for fov(Frames) to 24
+#Wondering why it's called FOV when it's affecting FPS -Chase
+func _ready():
+	set_fov(24)
+
+#Sets engine value for target fps to an inherited number.
+func set_fov(value: int):
+	Engine.set_target_fps(value)
+
+func set_post_process(enabled: bool):
+	post_process_lcd.set_shader_param("enabled", enabled)
+	post_process_blur.set_shader_param("enabled", enabled)
+	
+func set_lcd_opacity(value: float):
+	post_process_lcd.set_shader_param("lcd_opacity",value)
+	
+func set_lcd_scanline_gap(value: int):
+	post_process_lcd.set_shader_param("scanline_gap",value)
+
+func set_color_depth(value: int):
+	post_process_dither_band.set_shader_param("col_depth", value)
+
+func set_dither_banding(enabled: bool):
+	post_process_dither_band.set_shader_param("dither_banding", enabled)
+
+func set_fog_enabled(enabled: bool):
+	environment.set_fog_enabled(enabled)
+
+func set_fog_color(color: Color):
+	environment.set_fog_color(color)
+
+func set_fog_depth_begin(value: float):
+	environment.set_fog_depth_begin(value)
+
+func set_fog_depth_end(value: float):
+	environment.set_fog_depth_end(value)
+
+func set_ambient_light_color(color: Color):
+	environment.set_ambient_light_color(color)
+
+func set_ambient_energy(value: float):
+	environment.set_ambient_light_energy(value)
